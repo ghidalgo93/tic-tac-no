39fafb0b44760eb02e02f9b920433907513bd1bd
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-12-22 16:23:15 -0700

    add player name form and styles

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ const displayController = (() => {
       this.style.display = "none";
       const title = document.getElementById("title");
       title.classList.add("slide");
+      const nameForm = document.getElementById("name-form");
+      nameForm.classList.add("form-display");
+      const form = document.getElementById("form-container");
+      form.classList.remove("form-hide");
+      form.classList.add("form-display");
     },
     false
   );
