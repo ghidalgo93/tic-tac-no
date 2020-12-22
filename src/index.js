96a4b94c47c9426ef8327a678308e441f7437de7
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-12-22 12:58:56 -0700

    add display module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import "./styles.css";
 
-function slide() {
-  this.style.display = "none";
-  const title = document.getElementById("title");
-  title.classList.add("slide");
-}
+const displayController = (() => {
+  const startBtn = document.getElementById("start-btn");
 
-const startBtn = document.getElementById("start-btn");
-startBtn.addEventListener("click", slide, false);
+  startBtn.addEventListener(
+    "click",
+    // start animation
+    function () {
+      this.style.display = "none";
+      const title = document.getElementById("title");
+      title.classList.add("slide");
+    },
+    false
+  );
+})();
