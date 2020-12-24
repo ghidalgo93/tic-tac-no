b6b58ef539672259fd00e6db1f6c3d7f16df1276
ghidalgo93 <ghidalgo93@gmail.com>
2020-12-24 08:21:41 -0700

    add game module and refactor functions

--- src/displayController.js
+++ src/displayController.js
@@ -1,44 +1,33 @@
 import helpers from "./helpers";
 
 const displayController = (() => {
-  const startBtn = document.getElementById("start-btn");
-  const playBtn = document.getElementById("play-btn");
-  const formContainer = document.getElementById("form-container");
-
-  startBtn.addEventListener(
-    "click",
-    // start animation
-    function () {
-      this.style.display = "none";
-      const title = document.getElementById("title");
-      title.classList.add("slide-up");
-      formContainer.classList.remove("form-hide");
-      formContainer.classList.add("form-display");
-    },
-    false
-  );
-  playBtn.addEventListener(
-    "click",
-    function (e) {
-      e.preventDefault();
-      const inputsRaw = document.querySelectorAll(".inputs");
-      const inputs = [inputsRaw[0].value, inputsRaw[1].value];
-      if (helpers.verifyInputs(inputs)) {
-        // get names from form => send them to program somehow...
-        // Store names
-        // form container slide down
-        formContainer.classList.add("slide-down");
-        // game container fade in
-        const form = document.getElementById("name-form");
-        form.reset();
-      } else {
-        formContainer.classList.remove("shake");
-        void formContainer.offsetWidth;
-        formContainer.classList.add("shake");
-      }
-    },
-    false
-  );
+  function startAnimation() {
+    this.style.display = "none";
+    const title = document.getElementById("title");
+    title.classList.add("slide-up");
+    const formContainer = document.getElementById("form-container");
+    formContainer.classList.remove("form-hide");
+    formContainer.classList.add("form-display");
+  }
+  function play(e) {
+    e.preventDefault();
+    const formContainer = document.getElementById("form-container");
+    const inputsRaw = document.querySelectorAll(".inputs");
+    const inputs = [inputsRaw[0].value, inputsRaw[1].value];
+    if (helpers.verifyInputs(inputs)) {
+      // get names from form => send them to program somehow...
+      // Store names
+      formContainer.classList.add("slide-down");
+      // game container fade in
+      const form = document.getElementById("name-form");
+      form.reset();
+      return inputs;
+    }
+    formContainer.classList.remove("shake");
+    void formContainer.offsetWidth;
+    formContainer.classList.add("shake");
+  }
+  return { startAnimation, play };
 })();
 
 export default displayController;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./styles.css";
 import displayController from "./displayController";
 
+const game = (() => {
+  const startBtn = document.getElementById("start-btn");
+  const playBtn = document.getElementById("play-btn");
+  let names = [];
+
+  startBtn.addEventListener("click", displayController.startAnimation, false);
+  playBtn.addEventListener("click", (names = displayController.play), false);
+})();
